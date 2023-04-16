8e3008f9af23ad1a601726554ef6a55cc5b07b46
jay <jaeykweon@naver.com>
2023-04-16 18:04:00 +0900

    spring project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # developer-interview-assistant-api
+
 [토이 프로젝트] DIA - 개발자 면접 도우미 api
---
+++ src/main/kotlin/org/idd/dia/DeveloperInterviewAssistantApiApplication.kt
@@ -0,0 +1,11 @@
+package org.idd.dia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveloperInterviewAssistantApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeveloperInterviewAssistantApiApplication>(*args)
+}
