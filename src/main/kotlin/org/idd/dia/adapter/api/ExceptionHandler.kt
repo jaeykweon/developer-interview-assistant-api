0cb9e23c2e7cde94a628933d03d5927e94edd51f
jay <jaeykweon@naver.com>
2024-02-07 00:33:37 +0900

    익셉션 핸들러 일부 변경

--- src/main/kotlin/org/idd/dia/adapter/api/ExceptionHandler.kt
+++ src/main/kotlin/org/idd/dia/adapter/api/ExceptionHandler.kt
@@ -9,32 +9,35 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+/**
+ * 일부 익셉션 응답이 Not Found 인 이유는 악의적 사용자에게 정보를 주지 않기 위함.
+ */
 @RestControllerAdvice
 class ExceptionHandler {
     private val logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
 
     @ExceptionHandler(BadRequestException::class)
     fun handleBadRequestException(e: Exception): ApiResponse<Nothing> {
-        logger.info("Bad Request", e)
+        logger.error("Bad Request", e)
         return ApiResponse.badRequest("Bad Request")
     }
 
     @ExceptionHandler(UnAuthorizedException::class)
     fun handleUnAuthorizedException(e: Exception): ApiResponse<Nothing> {
-        logger.info("Unauthorized", e)
-        return ApiResponse.unauthorized("Unauthorized")
+        logger.error("Unauthorized", e)
+        return ApiResponse.notFound("Unauthorized")
     }
 
     @ExceptionHandler(NotFoundException::class)
     fun handleNotFoundException(e: Exception): ApiResponse<Nothing> {
-        logger.info("Not Found")
+        logger.error("Not Found")
         return ApiResponse.notFound("Not Found")
     }
 
     @ExceptionHandler(ForbiddenException::class)
     fun handleForbiddenException(e: Exception): ApiResponse<Nothing> {
-        logger.info("Forbidden Exception Occurred", e)
-        return ApiResponse.unauthorized("Forbidden")
+        logger.error("Forbidden Exception Occurred", e)
+        return ApiResponse.notFound("Forbidden")
     }
 
     @ExceptionHandler(InternalServerErrorException::class)
