d5e2ae9b137fd3d28e55592832f42c73d7bf4f70
jay <jaeykweon@naver.com>
2024-03-15 17:31:04 +0900

    로컬에서 prod를 덤프하는 기능을 추가합니다

---
+++ dump_prod_db_to_local_db.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#프로덕션 DB를 로컬 DB로 덮어씌우기 위한 스크립트입니다.
+#현재 로컬 DB를 삭제하고 프로덕션 DB를 덤프하여 로컬 DB로 복원합니다.
+
+echo "프로덕션 데이터베이스를 로컬 데이터베이스로 덮어 씌웁니다."
+
+echo "명령어 실행 오류 발생시 즉시 종료합니다."
+set -e
+
+DUMP_SOURCE_FILE="dump.sql"
+ENV_FILE="./.envs"
+
+echo "프로덕션 환경의 데이터베이스를 로컬 환경으로 덮어 씌웁니다."
+source $ENV_FILE
+
+echo "프로덕션 데이터베이스를 덤프합니다."
+PGPASSWORD=$PROD_PG_PASSWORD pg_dump \
+-h "$PROD_PG_HOST" \
+-p 5432 \
+-U "$PROD_PG_USER" \
+-d "$PROD_PG_DB_NAME" \
+> $DUMP_SOURCE_FILE
+
+echo "로컬 데이터베이스($LOCAL_DB_NAME)를 삭제하기 위해 기존 세션을 종료합니다."
+PGPASSWORD=$LOCAL_PG_PASSWORD psql -U postgres -c \
+"SELECT pg_terminate_backend(pg_stat_activity.pid) \
+FROM pg_stat_activity \
+WHERE pg_stat_activity.datname = '$LOCAL_DB_NAME';"
+
+echo "로컬 데이터베이스($LOCAL_DB_NAME)를 삭제합니다."
+PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+-U postgres \
+-c "DROP DATABASE $LOCAL_DB_NAME;"
+
+echo "로컬 데이터베이스($LOCAL_DB_NAME)를 생성합니다."
+PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+-U postgres \
+-c "CREATE DATABASE $LOCAL_DB_NAME;"
+
+echo "로컬 데이터베이스($LOCAL_DB_NAME)를 복원합니다."
+PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+-U postgres "$LOCAL_DB_NAME" < $DUMP_SOURCE_FILE
+
+echo "postgres 계정에 권한을 부여합니다."
+PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+-U postgres \
+-c "ALTER DATABASE $LOCAL_DB_NAME OWNER TO postgres;"
+
+echo "프로덕션 데이터베이스 -> 로컬 데이터베이스($LOCAL_DB_NAME) 복원이 완료되었습니다."
