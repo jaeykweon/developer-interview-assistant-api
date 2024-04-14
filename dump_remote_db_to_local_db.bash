645f47d49c32ba225bd7c562d76621aa628f822d
jay <jaeykweon@naver.com>
2024-04-15 00:02:22 +0900

    dump 스크립트와 template을 수정합니다

--- dump_remote_db_to_local_db.bash
+++ dump_remote_db_to_local_db.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#프로덕션 DB를 로컬 DB로 덮어씌우기 위한 스크립트입니다.
-#현재 로컬 DB를 삭제하고 프로덕션 DB를 덤프하여 로컬 DB로 복원합니다.
+#원격 DB를 로컬 DB로 덮어씌우기 위한 스크립트입니다.
+#현재 로컬 DB를 삭제하고 원격 DB를 덤프하여 로컬 DB로 복원합니다.
 
 echo "프로덕션 데이터베이스를 로컬 데이터베이스로 덮어 씌웁니다."
 
@@ -15,37 +15,37 @@ echo "프로덕션 환경의 데이터베이스를 로컬 환경으로 덮어 �
 source $ENV_FILE
 
 echo "프로덕션 데이터베이스를 덤프합니다."
-PGPASSWORD=$PROD_PG_PASSWORD pg_dump \
--h "$PROD_PG_HOST" \
+PGPASSWORD=$REMOTE_PG_PASSWORD pg_dump \
+-h "$REMOTE_PG_HOST" \
 -p 5432 \
--U "$PROD_PG_USER" \
--d "$PROD_PG_DB_NAME" \
+-U "$REMOTE_PG_USER" \
+-d "$REMOTE_PG_DB_NAME" \
 > $DUMP_SOURCE_FILE
 
 echo "로컬 데이터베이스($LOCAL_PG_DB_NAME)를 삭제하기 위해 기존 세션을 종료합니다."
-PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+PGPASSWORD=$REMOTE_PG_PASSWORD psql \
 -U "$LOCAL_PG_USER" \
 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) \
 FROM pg_stat_activity \
 WHERE pg_stat_activity.datname = '$LOCAL_PG_DB_NAME';"
 
 echo "로컬 데이터베이스($LOCAL_PG_DB_NAME)를 삭제합니다."
-PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+PGPASSWORD=$REMOTE_PG_PASSWORD psql \
 -U "$LOCAL_PG_USER" \
 -c "DROP DATABASE $LOCAL_PG_DB_NAME;"
 
 echo "로컬 데이터베이스($LOCAL_PG_DB_NAME)를 생성합니다."
-PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+PGPASSWORD=$REMOTE_PG_PASSWORD psql \
 -U "$LOCAL_PG_USER" \
 -c "CREATE DATABASE $LOCAL_PG_DB_NAME;"
 
 echo "로컬 데이터베이스($LOCAL_PG_DB_NAME)를 복원합니다."
-PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+PGPASSWORD=$REMOTE_PG_PASSWORD psql \
 -U "$LOCAL_PG_USER" \
 "$LOCAL_PG_DB_NAME" < $DUMP_SOURCE_FILE
 
 echo "$LOCAL_PG_USER 계정에 권한을 부여합니다."
-PGPASSWORD=$LOCAL_PG_PASSWORD psql \
+PGPASSWORD=$REMOTE_PG_PASSWORD psql \
 -U "$LOCAL_PG_USER" \
 -c "ALTER DATABASE $LOCAL_PG_DB_NAME OWNER TO $LOCAL_PG_USER;"
 
